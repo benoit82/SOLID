94b2cd501f01ada1ae5c918576d83de3c1e7a71c
Antoine07 <antoine.lucsko@gmail.com>
2021-09-06 12:02:39 +0200

    getter/setter

--- SOLID_PHP/SUPPORTS/Part1_relation/Exercices/Park/src/Vehicule.php
+++ SOLID_PHP/SUPPORTS/Part1_relation/Exercices/Park/src/Vehicule.php
@@ -2,4 +2,67 @@
 
 abstract class Vehicule {
     
+    private string $status;
+    private string $name;
+    private string $engine;
+
+    /**
+     * Get the value of status
+     */ 
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set the value of status
+     *
+     * @return  self
+     */ 
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of engine
+     */ 
+    public function getEngine()
+    {
+        return $this->engine;
+    }
+
+    /**
+     * Set the value of engine
+     *
+     * @return  self
+     */ 
+    public function setEngine($engine)
+    {
+        $this->engine = $engine;
+
+        return $this;
+    }
 }
