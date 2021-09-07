03fc2c45dfcf61ac6a8474cd85c072bd14e12aeb
Antoine07 <antoine.lucsko@gmail.com>
2021-09-07 11:12:07 +0200

    correction Parking

--- SOLID_PHP/SUPPORTS/Part1_relation/Exercices/ManageNews/app.php
+++ SOLID_PHP/SUPPORTS/Part1_relation/Exercices/ManageNews/app.php
@@ -5,9 +5,13 @@ spl_autoload_register(function ($class) {
 });
 
 $n1 = new ManageNews(new Log, "Article PHP");
+sleep(2);
 $n2 = new ManageNews(new Log, "Article MySQL");
+sleep(1);
 $n3 = new ManageNews(new Log, "Article JS");
+sleep(1);
 $n4 = new ManageNews(new Log, "Article MongoDB");
+sleep(1);
 $n5 = new ManageNews(new Log, "Article Python");
 
 var_dump(Log::getStorage());
